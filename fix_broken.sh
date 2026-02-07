9ef4a348e47c2e861e203e2472445d74f126f8e1
Mohammad Shaygan <mpgbieghesh468@gmail.com>
2026-02-07 19:07:14 +0330

    Add files via upload

---
+++ fix_broken.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Created By GodFather
+# Clean & Update your System for get broken packages
+sudo apt clean
+sudo apt update
+
+# Reconfigure your PKGS
+sudo apt --fix-broken install
+sudo dpkg --configure -a
+
+# is it fixed
+sudo apt upgrade -y
+
+# Otherwise, if it was not fixed:
+sudo apt install --reinstall dpkg
+sudo apt install --reinstall $(dpkg -l | awk '/^ii/ {print $2}')
+sudo rm -rf /var/crash/*
+
+# reboot after fix:
+if [[ "$?" -eq 0 ]]; then
+    reboot
+fi
